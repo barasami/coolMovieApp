aee9d26fb5827a2e2facc7ada6dff5a1004356ae
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-01-05 21:31:03 +0300

    Edited my title

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Footer.css'
 
+let myYear=new Date().getFullYear()
+
 function Footer() {
   return (
     <footer className='footer'>
@@ -9,7 +11,7 @@ function Footer() {
           <a className='about' href='https://barasamicheal.netlify.app' rel="noreferrer" target='_blank'>About Me</a>
         </div>
         <div className='copyright'>
-          <p>&copy;Movies Hub,2023 All rights Reserved</p>
+          <p>&copy;Movies Hub,{myYear} All rights Reserved</p>
         </div>
       </div>
     </footer>
